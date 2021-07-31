d5a35b8ac3de1ec27551f152c9502d7e05c6f9ab
keshavarz13 <keshavarz.ma13@gmail.com>
2021-07-31 19:40:57 +0430

    Make username, email, phone number unique key

--- Identity/Data/ApplicationDbContext.cs
+++ Identity/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;  
 using Microsoft.EntityFrameworkCore;  
 
@@ -6,12 +7,19 @@ namespace Identity.Data
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>  
     {  
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)  
-        {  
-  
+        {
         }  
         protected override void OnModelCreating(ModelBuilder builder)  
         {  
             base.OnModelCreating(builder);  
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.HasIndex(u => u.UserName).IsUnique();
+                entity.HasIndex(u => u.NormalizedUserName).IsUnique();
+                entity.HasIndex(u => u.Email).IsUnique();
+                entity.HasIndex(u => u.NormalizedEmail).IsUnique();
+                entity.HasIndex(u => u.PhoneNumber).IsUnique();
+            });
         }  
     }  
 }
