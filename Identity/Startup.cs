11e115337e1fb462e6bb933ef2c7241d1f9b33a8
keshavarz13 <keshavarz.ma13@gmail.com>
2021-07-31 20:25:45 +0430

    Config authorization for swagger

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -80,6 +80,27 @@ namespace Identity
                     Title = "My Awesome API",
                     Version = "v1"
                 });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement  
+                {  
+                    {  
+                        new OpenApiSecurityScheme  
+                        {  
+                            Reference = new OpenApiReference  
+                            {  
+                                Type = ReferenceType.SecurityScheme,  
+                                Id = "Bearer"  
+                            }  
+                        },  
+                        new string[] {}
+                    }  
+                });
             });
         }  
   
