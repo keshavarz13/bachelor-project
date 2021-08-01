c0715a0ec490e8a0a02019f0ae8b187161139295
keshavarz13 <keshavarz.ma13@gmail.com>
2021-08-01 22:36:33 +0430

    Add data migrations

---
+++ Notification/Data/Migrations/20210801180453_InitialMigration.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Notification.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Email",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReceiverEmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailSubject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailStatus = table.Column<int>(type: "int", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReceivingTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SubjectId = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Subject = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Email", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sms",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReceiverPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SmsStatus = table.Column<int>(type: "int", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReceivingTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SubjectId = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Subject = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sms", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Email");
+
+            migrationBuilder.DropTable(
+                name: "Sms");
+        }
+    }
+}
--- Notification/Data/NotificationDbContext.cs
+++ Notification/Data/NotificationDbContext.cs
@@ -1,15 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using Notification.Models;
 
 namespace Notification.Data
 {
-    public class NotificationDbContext : DbContext  
-    {  
-        public NotificationDbContext(DbContextOptions<NotificationDbContext> options) : base(options)  
+    public class NotificationDbContext : DbContext
+    {
+        public NotificationDbContext(DbContextOptions<NotificationDbContext> options) : base(options)
         {
-        }  
-        protected override void OnModelCreating(ModelBuilder builder)  
-        {  
-            base.OnModelCreating(builder);
-        }  
-    }  
+        }
+
+        public DbSet<Email> Email { get; set; }
+        public DbSet<Sms> Sms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Email>();
+            builder.Entity<Sms>();
+        }
+    }
 }
