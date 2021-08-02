db89d80cd2d7d88a9ab57fe3ebd13ae501e642e0
keshavarz13 <keshavarz.ma13@gmail.com>
2021-08-02 23:41:36 +0430

    Add get sms by filter to sms service

--- Notification/Services/ISmsService.cs
+++ Notification/Services/ISmsService.cs
@@ -1,12 +1,17 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Notification.Controller.Contracts;
 using Notification.Models;
 
 namespace Notification.Services
 {
     public interface ISmsService
     {
-        Task<Sms> SendSms(Sms sms);
-        Task<List<Sms>> GetSms();
+        Task<SmsOutputDto> SendSms(Sms sms);
+        Task<List<SmsOutputDto>> GetSms();
+        Task<List<SmsOutputDto>> GetSmsByFilter(string phoneNumber, DateTime? startCreationTime,
+            DateTime? endCreationTime, DateTime? startReceivingTime, DateTime? endReceivingTime, string smsStatus);
+        Task<SmsOutputDto> GetSmsById(long id);
     }
 }
--- Notification/Services/Imp/SmsService.cs
+++ Notification/Services/Imp/SmsService.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Notification.Controller.Contracts;
 using Notification.Models;
 using Notification.Repository;
 
@@ -18,14 +22,59 @@ namespace Notification.Services.Imp
             _mapper = mapper;
         }
 
-        public async Task<Sms> SendSms(Sms sms)
+        public async Task<SmsOutputDto> SendSms(Sms sms)
+        {
+            sms = await SendToprovider(sms);
+            return _mapper.Map<SmsOutputDto>(await _smsRepository.AddAsync(sms));
+        }
+
+        public async Task<List<SmsOutputDto>> GetSms()
+        {
+            return _mapper.Map<List<SmsOutputDto>>(
+                await _smsRepository.GetQueryableAsync().OrderByDescending(x => x.Id).ToListAsync());
+        }
+
+        public async Task<List<SmsOutputDto>> GetSmsByFilter(string phoneNumber, DateTime? startCreationTime,
+            DateTime? endCreationTime, DateTime? startReceivingTime, DateTime? endReceivingTime, string smsStatus)
+        {
+            var queryable = _smsRepository.GetQueryableAsync();
+            if (!string.IsNullOrEmpty(phoneNumber))
+                queryable = queryable.Where(x => x.ReceiverPhoneNumber == phoneNumber);
+
+            if (!string.IsNullOrEmpty(smsStatus))
+                queryable = queryable.Where(x =>
+                    x.SmsStatus == (SmsStatus) Enum.Parse(typeof(SmsStatus), smsStatus, true));
+
+            if (startCreationTime != null)
+                queryable = queryable.Where(x => x.CreationTime >= startCreationTime);
+
+            if (endCreationTime != null)
+                queryable = queryable.Where(x => x.CreationTime <= endCreationTime);
+
+            if (startReceivingTime != null)
+                queryable = queryable.Where(x => x.ReceivingTime >= startReceivingTime);
+
+            if (endReceivingTime != null)
+                queryable = queryable.Where(x => x.ReceivingTime <= endReceivingTime);
+
+            queryable = queryable.OrderByDescending(x => x.Id);
+            return _mapper.Map<List<SmsOutputDto>>(await queryable.ToListAsync());
+        }
+
+        public async Task<SmsOutputDto> GetSmsById(long id)
+        {
+            return _mapper.Map<SmsOutputDto>(await _smsRepository.GetQueryableAsync().Where(x => x.Id == id)
+                .FirstOrDefaultAsync());
+        }
+
+        private async Task<Sms> SaveToDatabase(Sms sms)
         {
             return await _smsRepository.AddAsync(sms);
         }
-        
-        public async Task<List<Sms>> GetSms()
+
+        private async Task<Sms> SendToprovider(Sms sms)
         {
-            return await _smsRepository.GetQueryableAsync().ToListAsync();
+            return sms;
         }
     }
 }
