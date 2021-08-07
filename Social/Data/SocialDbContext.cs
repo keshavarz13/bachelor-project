2eb09f29f089274ad0f5d72c8f1ac1bfc8c30313
keshavarz13 <keshavarz.ma13@gmail.com>
2021-08-07 19:42:49 +0430

    Add db sets

--- Social/Data/SocialDbContext.cs
+++ Social/Data/SocialDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Social.Models;
 
 namespace Social.Data
 {
@@ -8,8 +9,16 @@ namespace Social.Data
         {
         }
 
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Follow> Follow { get; set; }
+        public DbSet<Post> Post { get; set; }
+        public DbSet<Read> Read { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<Book>();
+            builder.Entity<Post>();
+            builder.Entity<Follow>();
+            builder.Entity<Read>();
         }
     }
 }
