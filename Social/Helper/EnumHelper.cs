52e056a2f7ca608e1f85da80f0bea026b55be825
keshavarz13 <keshavarz.ma13@gmail.com>
2021-08-09 19:23:10 +0430

    Add helper for read enum descriptions

---
+++ Social/Helper/EnumHelper.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel;
+
+namespace Social.Helper
+{
+    public static class EnumHelper
+    {
+        public static string GetEnumDescription(Enum enumValue)
+        {
+            var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+            var descriptionAttributes =
+                (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return descriptionAttributes.Length > 0 ? descriptionAttributes[0].Description : enumValue.ToString();
+        }
+    }
+}
